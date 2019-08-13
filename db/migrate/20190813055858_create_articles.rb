d9bc7990a6b5313ce02ccbf078df67b3eb628ca0
suzuki05 <>
2019-08-13 15:09:27 +0900

    Articleのマイグレーションファイルを編集

--- db/migrate/20190813055858_create_articles.rb
+++ db/migrate/20190813055858_create_articles.rb
@@ -1,7 +1,12 @@
 class CreateArticles < ActiveRecord::Migration[5.1]
   def change
     create_table :articles do |t|
-
+      t.string      :title
+      t.text        :article
+      t.string      :category
+      t.string      :author
+      t.integer     :state
+      t.text        :comment
       t.timestamps
     end
   end
