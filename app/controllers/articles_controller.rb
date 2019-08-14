df19cedf24304e9aedf818497e015041c1ba0a61
suzuki05 <>
2019-08-14 20:06:55 +0900

    記事新規作成ページ設定

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,19 @@
 class ArticlesController < ApplicationController
   def index
-
+    @articles = Article.includes(:user).order("created_at DESC")
   end
 
   def new
-
+    @article = Article.new
   end
 
   def create
+    binding.pry
+    Article.create(create_params)
+  end
 
+  private
+  def create_params
+    params.permit(:title, :category, :state, :article).merge(author: current_user.id)
   end
 end
