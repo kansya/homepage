ddd06e8b60794c0878096341001acdc87ad93cee
suzuki05 <>
2019-08-15 21:25:31 +0900

    コメント機能を設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,11 @@
 class CommentsController < ApplicationController
+  def create
+    @comment = Comment.create(comment_params)
+    redirect_to "/articles/#{@comment.article.id}"
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text, :nickname).merge(article_id: params[:article_id])
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190814183808) do
+ActiveRecord::Schema.define(version: 20190815115731) do
 
   create_table "articles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "title"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20190814183808) do
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nickname"
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
