e8e4b1bdade7142445f4ffc279ab2cbf933d9b71
suzuki05 <>
2019-08-15 22:07:05 +0900

    before_actionの設定

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
+
   def index
     @articles = Article.includes(:user).page(params[:page]).per(5).order("created_at DESC")
   end
@@ -42,5 +44,9 @@ class ArticlesController < ApplicationController
   private
   def create_params
     params.require(:article).permit(:title, :category, :state, :article).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    redirect_to :action => "index" unless user_signed_in?
   end
 end
